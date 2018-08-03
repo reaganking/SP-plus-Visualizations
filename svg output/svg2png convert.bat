a4954d9a7fca503038f8ef09e85643c9f916f1e2
Evan Hartman <evan.robert.hartman@gmail.com>
2018-08-03 11:42:38 -0500

    Updated folder structure for easier use.

---
+++ svg output/svg2png convert.bat
@@ -0,0 +1,10 @@
+@echo off
+for %%f in (%*) do (
+    echo %%~f
+    "C:\Program Files\Inkscape\inkscape.exe" ^
+      -z ^
+      --export-background-opacity=0 ^
+      --export-png="%%~dpnf.png" ^
+      --file="%%~f"
+
+)
