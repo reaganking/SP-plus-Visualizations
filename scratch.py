86340ba77bc38241a9787dda11ced02f6b5d79df
Evan Hartman <evan.robert.hartman@gmail.com>
2018-08-06 13:45:22 -0500

    Remainder of pre season data added.

--- scratch.py
+++ scratch.py
@@ -1,3 +1,4 @@
+import csv
 import json
 
 from cluster import Cluster
@@ -6,28 +7,28 @@ from team import Team
 
 with open("schedule.json", "r") as file:
     schedule = json.load(file)
+'''
+team = 'louisiana'
+win_prob = [0.73, 0.03, 0.53, 0.01, 0.47, 0.43, 0.15, 0.24, 0.20, 0.51, 0.47, 0.35]
+for i in range(len(win_prob)):
+    schedule[team]['schedule'][i]['spplus'] = [win_prob[i]]
 
-win_prob = [.99, .25, .90, .14, .66, .75, .53, .34, .21, .56, .86, .49]
-
-for i in range(len(schedule['texas am']['schedule'])):
-    schedule['texas am']['schedule'][i]['spplus'] = [win_prob[i]]
 with open('schedule.json', 'w') as file:
     json.dump(schedule, file, indent=4, sort_keys=True)
-
-
-scale = 'team'
+'''
+scale = 'red-green'
 
 pfive = ['atlantic coast', 'big ten', 'big 12', 'pac 12', 'southeastern']
-gfive = ['american athletic', 'conference usa', 'mid american', 'mountain west', 'sunbelt']
+gfive = ['american athletic', 'conference usa', 'mid american', 'mountain west', 'sun belt']
 fcs = pfive + gfive
 
-for conference in pfive:
+for conference in gfive:
     Conference(name=conference, schedule=schedule).make_standings_projection_graph(absolute=False, file=conference,
                                                                                    scale=scale)
 
 Cluster(schedule=schedule,
-        teams=[x for x in schedule if schedule[x]['conference'] in pfive]).make_standings_projection_graph(
-    absolute=False, file='p5', scale=scale)
+        teams=[x for x in schedule if schedule[x]['conference'] in fcs]).make_standings_projection_graph(
+    absolute=False, file='fcs', scale=scale)
 
 for team in schedule:
     if schedule[team]['conference'] in ['atlantic coast', 'big ten', 'big 12', 'pac 12', 'southeastern']:
